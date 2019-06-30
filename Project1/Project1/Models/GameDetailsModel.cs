be9f26fcfe8f3b7afa8ecd3b4d676ae5e51b416e
rainsky5842 <w584210119@gmail.com>
2019-06-30 13:03:55 -0400

    Create Project1

---
+++ Project1/Project1/Models/GameDetailsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Models
+{
+    public class GameDetailsModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
